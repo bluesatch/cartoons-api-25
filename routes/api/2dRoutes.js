ef40da69581843c96e4b2d8ea9c3d103afba4355
bluesatch <satchwerk@gmail.com>
2025-04-04 15:04:01 -0500

    first commit

---
+++ routes/api/2dRoutes.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
+
+let count
+// get data using axios
+axios.get('https://api.sampleapis.com/cartoons/cartoons2D')
+    .then(res => count = res.data.length)
+
+// localhost:3000/cartoons2D
+router.get('/', (req, res)=> {
+
+    // get the url
+    const url = 'https://api.sampleapis.com/cartoons/cartoons2D'
+
+    axios.get(url).then(response => {
+    
+        res.render('pages/cartoons', {
+            title: '2D Cartoons',
+            name: '2D Cartoons',
+            data: response.data
+        })
+    })
+})
+
+// single page
+// localhost:3000/cartoons2d/id
+router.get('/:id', (req, res)=> {
+    const id = req.params.id 
+
+    const url = `https://api.sampleapis.com/cartoons/cartoons2D/${id}`
+
+    axios.get(url).then(resp => {
+        
+        const data = resp.data
+        res.render('pages/cartoonSingle', {
+            title: data.title,
+            name: data.title,
+            data: data,
+            count
+        })
+    })
+})
+
+module.exports = router
---
+++ routes/router.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
+
+router.use(express.static('public'))
+
+const twoDRoutes = require('./api/2dRoutes')
+router.use('/cartoons2d', twoDRoutes)
+
+// home page
+router.get('/', (req, res)=> {
+    let randomCartoon
+
+    axios.get('https://api.sampleapis.com/cartoons/cartoons2D/')
+        .then(resp => {
+
+            randomCartoon = resp.data[Math.floor(Math.random() * resp.data.length)]
+            res.render('pages/home', {
+                title: 'My Cartoons Website',
+                name: 'Cartoons',
+                cartoon: randomCartoon
+            })
+        })
+
+})
+
+module.exports = router
